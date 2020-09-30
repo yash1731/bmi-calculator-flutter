2dd68484dac22031e72a52da82d9f5c0e21c4718
Yashpal Dewangan <yash.dewangan82@gmail.com>
2020-09-30 09:59:21 +0100

    first commit

---
+++ lib/reusable_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+//! creating reusable card to if any code we are doing same again and angain
+//! remove defalut constructor from code
+//! add property to if we want ot change in color and to use it need to create constructor
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour, this.cardChild, this.onPress});
+  final Color colour;
+  final Widget cardChild;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
+      ),
+    );
+  }
+}
